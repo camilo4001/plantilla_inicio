7181eec637840f58592ef885847d221c5af2a679
camilo.rojas <camilo.rojas@xertica.com>
2022-01-26 09:12:14 -0500

    prueba

--- main/simple_ota_example.c
+++ main/simple_ota_example.c
@@ -61,7 +61,7 @@ void simple_ota_example_task(void *pvParameter)
     ESP_LOGI(TAG, "Starting OTA example");
 
     esp_http_client_config_t config = {
-        .url = "innovacionesco.com:3389",
+        .url = "https://innovacionesco.com:3389/firmware",
         .cert_pem = (char *)server_cert_pem_start,
         .event_handler = _http_event_handler,
     };
