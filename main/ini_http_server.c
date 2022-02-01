27f3c22b876d5ff6c92a537fbce5d74e2e8bc51f
camilo.rojas <camilo.rojas@xertica.com>
2022-01-31 19:44:38 -0500

    prueba

--- main/ini_http_server.c
+++ main/ini_http_server.c
@@ -28,7 +28,6 @@
 #include "lwip/netdb.h"
 #include "lwip/api.h"
 #include "principal.h"
-#include "dht11.h"
 
 int state = 0;
 const static char http_html_hdr[] = "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
@@ -36,23 +35,7 @@ const static char comando_response[] = "<html><head>   <title>Control</title>   
 //const static char http_index_hml[] = "<html><head><title>Control</title></head><body><h1>Control</h1><a href=\"h\">On</a><br><a href=\"l\">Off</a></body></html>";
 
 
-//VARIBLES DE SENSORES
-extern int sensor_suelo;
-extern struct dht11_reading dht11_datos;
-// Luz 1
-extern uint8_t  pwm_luz_sal1;
-extern uint8_t  pwm_horario_encendido;
-//SAL1
-extern uint8_t  intervalo_encendido_sal1;
-extern uint8_t duracion_encedido_salida1;
 
-//registro salidas
-extern uint8_t registro_salidas; // cada bit representa el estado de una salida 1=prendido 0=Apagado  bits altos
-extern uint8_t salidas_activas;  // bit bajos
-//Fecha
-extern char fecha_str[64];
-extern uint8_t comando_env[];
-extern uint8_t ant_comando_env[];
 //#define EXAMPLE_WIFI_SSID "Tomas_2G"
 //#define EXAMPLE_WIFI_PASS "tomas4982"
 //const int DIODE_PIN = 5;
@@ -60,55 +43,6 @@ extern uint8_t ant_comando_env[];
 //static EventGroupHandle_t wifi_event_group;
 //const int CONNECTED_BIT = BIT0;
 
-extern char fecha_reg1[64];
-extern char fecha_reg2[64];
-extern char fecha_reg3[64];
-
-
-extern char rep_str_sal1a[64];
-extern char rep_str_sal1b[64];
-extern char rep_str_sal1c[64];
-
-extern char rep_str_sal2a[64];
-extern char rep_str_sal2b[64];
-extern char rep_str_sal2c[64];
-
-extern char rep_str_sal3a[64];
-extern char rep_str_sal3b[64];
-extern char rep_str_sal3c[64];
-
-extern char rep_str_sal4a[64];
-extern char rep_str_sal4b[64];
-extern char rep_str_sal4c[64];
-
-extern uint8_t	hist1temp;
-extern uint8_t	hist1tempdec;
-extern uint8_t	hist1humed;
-extern uint8_t	hist1suelo;
-
-extern uint8_t	hist2temp;
-extern uint8_t	hist2tempdec;
-extern uint8_t	hist2humed;
-extern uint8_t	hist2suelo;
-
-extern uint8_t	hist3temp;
-extern uint8_t	hist3tempdec;
-extern uint8_t	hist3humed;
-extern uint8_t	hist3suelo;
-
-//extern int final_sensor_suelo;
-extern uint8_t suelo_medida_base;
-extern uint8_t umbral_disparos_suelo;
-extern uint8_t numero_disparos_suelo;
-extern uint8_t modo_func_suelo;
-
-extern uint8_t estado_comando;
-
-extern uint8_t tiempo_falt_act_sal1;
-
-extern uint8_t horaaini;
-extern uint8_t minutoini;
-extern uint8_t esperando_ini;
 
 static void
 http_server_netconn_serve(struct netconn *conn)
@@ -172,7 +106,7 @@ http_server_netconn_serve(struct netconn *conn)
        if(buf[5]=='?' && ant_comando_env[0] == DEFECTO_COMANDO){
 		   
 		   printf("Entra a encontrar valores \n");
-		   	
+		   	/*
 			if(buf[6]=='h' && buf[7]=='a' && buf[8]=='b'){
 				sscanf(buf, FORMATO_PETICION_WEB_HAB,&habvsal1,&habvsal2,&habvsal3,&habvsal4);
 				printf("valor encontrados vs1 = %d vs2 = %d vs3 = %d vs4 = %d\n",habvsal1,habvsal2,habvsal3,habvsal4);
@@ -251,11 +185,11 @@ http_server_netconn_serve(struct netconn *conn)
 				}
 				//else{esperando_ini=0;}
 			}
-			
+			*/
 		
-		  aplicar_comandos(comando_env[0],comando_env[1]);
+		  //aplicar_comandos(comando_env[0],comando_env[1]);
 			
-		  ESP_LOGI("HTMLINFO", "\n ****** HTML A MOSTRAR \"%s\" ***tamaño %d************\n", comando_response,sizeof(comando_response)-1);
+		  //ESP_LOGI("HTMLINFO", "\n ****** HTML A MOSTRAR \"%s\" ***tamaño %d************\n", comando_response,sizeof(comando_response)-1);
 
 		  netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
 		  netconn_write(conn, comando_response, sizeof(comando_response)-1, NETCONN_NOCOPY);
@@ -264,260 +198,6 @@ http_server_netconn_serve(struct netconn *conn)
 	   }
 	   else{
 		   
-		   if(intervalo_encendido_sal1==0){
-			   enc_tiempo[0] = 'A';
-			   enc_tiempo[1] = 'p';
-			   enc_tiempo[2] = 'a';
-			   enc_tiempo[3] = 'g';
-			   enc_tiempo[4] = 'a';
-			   enc_tiempo[5] = 'd';
-			   enc_tiempo[6] = 'o';
-			   enc_tiempo[7] = ' ';
-			   enc_tiempo[8] = ' ';
-			   //enc_tiempo[0] = "Apagado";
-		   }
-		   if(suelo_medida_base==0){
-			   enc_sensor[0] = 'A';
-			   enc_sensor[1] = 'p';
-			   enc_sensor[2] = 'a';
-			   enc_sensor[3] = 'g';
-			   enc_sensor[4] = 'a';
-			   enc_sensor[5] = 'd';
-			   enc_sensor[6] = 'o';
-			   enc_sensor[7] = ' ';
-			   enc_sensor[8] = ' ';
-			   //enc_sensor = "Apagado";
-		   }
-		   if(modo_func_suelo==1){
-			   modo_sensor[0] = 'A';
-			   modo_sensor[1] = 'c';
-			   modo_sensor[2] = 't';
-			   modo_sensor[3] = ' ';
-			   modo_sensor[4] = 'S';
-			   modo_sensor[5] = 'a';
-			   modo_sensor[6] = 'l';
-			   modo_sensor[7] = 'i';
-			   modo_sensor[8] = 'd';
-			   modo_sensor[9] = 'a';
-			   modo_sensor[10] = ' ';
-			   modo_sensor[11] = ' ';
-			   //enc_sensor = "Apagado";
-		   }	
-		   
-		   if(esperando_ini==1){
-			   hora_inicio[0] = 'P';
-			   hora_inicio[1] = 'e';
-			   hora_inicio[2] = 'n';
-			   hora_inicio[3] = 'd';
-			   hora_inicio[4] = 'i';
-			   hora_inicio[5] = 'e';
-			   hora_inicio[6] = 'n';
-			   hora_inicio[7] = 't';
-			   hora_inicio[8] = 'e';
-			   hora_inicio[9] = ' ';
-			   hora_inicio[10] = ' ';
-			   hora_inicio[11] = ' ';
-		   }			   
-		   if(esperando_ini==2){
-			   hora_inicio[0] = 'E';
-			   hora_inicio[1] = 'n';
-			   hora_inicio[2] = 'c';
-			   hora_inicio[3] = 'o';
-			   hora_inicio[4] = 'n';
-			   hora_inicio[5] = 't';
-			   hora_inicio[6] = 'r';
-			   hora_inicio[7] = 'a';
-			   hora_inicio[8] = 'd';
-			   hora_inicio[9] = 'a';
-			   hora_inicio[10] = ' ';
-			   hora_inicio[11] = ' ';
-		   }		   
-		   if(esperando_ini==3){
-			   hora_inicio[0] = 'E';
-			   hora_inicio[1] = 'n';
-			   hora_inicio[2] = 'v';
-			   hora_inicio[3] = 'i';
-			   hora_inicio[4] = 'a';
-			   hora_inicio[5] = 'n';
-			   hora_inicio[6] = 'd';
-			   hora_inicio[7] = 'o';
-			   hora_inicio[8] = ' ';
-			   hora_inicio[9] = ' ';
-			   hora_inicio[10] = ' ';
-			   hora_inicio[11] = ' ';
-		   }
-		   
-		   
-
-		  if((salidas_activas&0x08) == 0){
-			sal_hab1 = 0;
-		  }
-		  if((salidas_activas&0x04) == 0){
-			sal_hab2 = 0;
-		  }
-		  if((salidas_activas&0x02) == 0){
-			sal_hab3 = 0;
-		  }
-		  if((salidas_activas&0x01) == 0){
-			sal_hab4 = 0;		  
-		  }	
-		  if((registro_salidas&0x80) == 0){
-			sal_activa1 = 0;
-		  }
-		  if((registro_salidas&0x40) == 0){
-			sal_activa2 = 0;
-		  }
-		  if((registro_salidas&0x20) == 0){
-			sal_activa3 = 0;
-		  }
-		  if((registro_salidas&0x10) == 0){
-			sal_activa4 = 0;
-		  }
-
-		  if(comando_env[0] == ACTIVAR_SALIDA){
-			comando_actual[0] = 'A';
-			comando_actual[1] = 'c';
-			comando_actual[2] = 't';
-			comando_actual[3] = ' ';
-			comando_actual[4] = 'S';
-			comando_actual[5] = 'a';
-			comando_actual[6] = 'l';
-			comando_actual[7] = 'i';
-			comando_actual[8] = 'd';
-			comando_actual[9] = 'a';
-		  }
-		  if(comando_env[0] == INHABILITAR_SALIDAS){
-			comando_actual[0] = 'D';
-			comando_actual[1] = 'e';
-			comando_actual[2] = 's';
-			comando_actual[3] = ' ';
-			comando_actual[4] = 'S';
-			comando_actual[5] = 'a';
-			comando_actual[6] = 'l';
-			comando_actual[7] = 'i';
-			comando_actual[8] = 'd';
-			comando_actual[9] = 'a';
-		  }		  
-		  if(comando_env[0] == CAMB_REG_DUR_SAL1){
-			comando_actual[0] = 'D';
-			comando_actual[1] = 'u';
-			comando_actual[2] = 'r';
-			comando_actual[3] = ' ';
-			comando_actual[4] = 'S';
-			comando_actual[5] = 'a';
-			comando_actual[6] = 'l';
-			comando_actual[7] = 'i';
-			comando_actual[8] = 'd';
-			comando_actual[9] = 'a';
-		  }
-		  if(comando_env[0] == CAMB_REG_INTERVALO_SAL1){
-			comando_actual[0] = 'I';
-			comando_actual[1] = 'n';
-			comando_actual[2] = 't';
-			comando_actual[3] = ' ';
-			comando_actual[4] = 'S';
-			comando_actual[5] = 'a';
-			comando_actual[6] = 'l';
-			comando_actual[7] = 'i';
-			comando_actual[8] = 'd';
-			comando_actual[9] = 'a';
-		  }
-		  if(comando_env[0] == CAMB_UMBRAL_SUELO){
-			comando_actual[0] = 'U';
-			comando_actual[1] = 'm';
-			comando_actual[2] = 'b';
-			comando_actual[3] = ' ';
-			comando_actual[4] = 'S';
-			comando_actual[5] = 'u';
-			comando_actual[6] = 'e';
-			comando_actual[7] = 'l';
-			comando_actual[8] = 'o';
-			comando_actual[9] = ' ';
-		  }
-		  if(comando_env[0] == CAMB_DISPARADOR_SUELO){
-			comando_actual[0] = 'D';
-			comando_actual[1] = 'i';
-			comando_actual[2] = 's';
-			comando_actual[3] = 'p';
-			comando_actual[4] = ' ';
-			comando_actual[5] = 'S';
-			comando_actual[6] = 'u';
-			comando_actual[7] = 'e';
-			comando_actual[8] = 'l';
-			comando_actual[9] = 'o';
-		  }		  
-		  
-		  if(comando_env[0] == SINC_HORA_ESP){
-			comando_actual[0] = 'S';
-			comando_actual[1] = 'i';
-			comando_actual[2] = 'n';
-			comando_actual[3] = 'c';
-			comando_actual[4] = ' ';
-			comando_actual[5] = 'H';
-			comando_actual[6] = 'o';
-			comando_actual[7] = 'r';
-			comando_actual[8] = 'a';
-			comando_actual[9] = ' ';
-		  }		  
-		  if(comando_env[0] == SINC_HORA_INI){
-			comando_actual[0] = 'A';
-			comando_actual[1] = 'c';
-			comando_actual[2] = 't';
-			comando_actual[3] = ' ';
-			comando_actual[4] = 'S';
-			comando_actual[5] = 'i';
-			comando_actual[6] = 'n';
-			comando_actual[7] = 'c';
-			comando_actual[8] = ' ';
-			comando_actual[9] = ' ';
-		  }
-		  
-
-		  if(estado_comando == COMANDO_ENVIADO){
-			comando_estado[0] = 'E';
-			comando_estado[1] = 'n';
-			comando_estado[2] = 'v';
-			comando_estado[3] = 'i';
-			comando_estado[4] = 'a';
-			comando_estado[5] = 'd';
-			comando_estado[6] = 'o';
-			comando_estado[7] = ' ';
-
-		  }		  
-		  if(estado_comando == COMANDO_GUARDADO){
-			comando_estado[0] = 'G';
-			comando_estado[1] = 'u';
-			comando_estado[2] = 'a';
-			comando_estado[3] = 'r';
-			comando_estado[4] = 'd';
-			comando_estado[5] = 'a';
-			comando_estado[6] = 'd';
-			comando_estado[7] = 'o';
-
-		  }
-		  if(estado_comando == COMANDO_APLICADO){
-			comando_estado[0] = 'A';
-			comando_estado[1] = 'p';
-			comando_estado[2] = 'l';
-			comando_estado[3] = 'i';
-			comando_estado[4] = 'c';
-			comando_estado[5] = 'a';
-			comando_estado[6] = 'd';
-			comando_estado[7] = 'o';
-
-		  }
-		  if(estado_comando == COMANDO_FALLIDO){
-			comando_estado[0] = 'F';
-			comando_estado[1] = 'a';
-			comando_estado[2] = 'l';
-			comando_estado[3] = 'l';
-			comando_estado[4] = 'i';
-			comando_estado[5] = 'd';
-			comando_estado[6] = 'o';
-			comando_estado[7] = ' ';
-		  }		  
-		  
-		  
 		  asprintf(&html_message, FORMATO_HTML,dht11_datos.temperature,dht11_datos.temperature_decimal,dht11_datos.humidity,sensor_suelo,suelo_medida_base,umbral_disparos_suelo,modo_sensor,horaaini,minutoini,hora_inicio,duracion_encedido_salida1,intervalo_encendido_sal1,enc_tiempo,enc_sensor,tiempo_falt_act_sal1,var_n_disparos_suelo,comando_actual,comando_env[1],comando_estado,fecha_reg1,hist1temp,hist1tempdec,hist1humed,hist1suelo,fecha_reg2,hist2temp,hist2tempdec,hist2humed,hist2suelo,fecha_reg3,hist3temp,hist3tempdec,hist3humed,hist3suelo,rep_str_sal1a,rep_str_sal1b,rep_str_sal1c,rep_str_sal2a,rep_str_sal2b,rep_str_sal2c,rep_str_sal3a,rep_str_sal3b,rep_str_sal3c,rep_str_sal4a,rep_str_sal4b,rep_str_sal4c,sal_activa1,sal_activa2,sal_activa3,sal_activa4,sal_hab1,sal_hab2,sal_hab3,sal_hab4);
 
 		  //ESP_LOGI("HTMLINFO", "\n ****** HTML A MOSTRAR \"%s\" ***tamaño %d************\n", html_message,strlen(html_message)-1);
