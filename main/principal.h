7a959c7b8ca0df5e65507fa2b7c68e982eba1a86
camilo.rojas <camilo.rojas@xertica.com>
2022-01-31 20:13:29 -0500

    pruebas

--- main/principal.h
+++ main/principal.h
@@ -50,7 +50,6 @@
 void http_server_ini();
 void init_wifi();
 void ble_inicio();
-
 //*****
 void tft_init();
 void tft_init(void);
@@ -58,4 +57,5 @@ void fillScreen(uint16_t color);
 void testdrawtext(char *text, uint16_t color);
 void escribir_algo(char * text,int text_len,int renglon,uint8_t tam_tex);
 //*****
+void enviar_valor_spi(const uint8_t cmd)
 
