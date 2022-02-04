4fca947a89affb00e72f5f0e505f0545699be915
camilo.rojas <camilo.rojas@xertica.com>
2022-02-04 16:01:38 -0500

    pruebas

--- main/simple_ota_example.c
+++ main/simple_ota_example.c
@@ -87,7 +87,7 @@ void simple_get_example_task(void *pvParameter)
     // GET
     esp_err_t err = esp_http_client_perform(client);
     if (err == ESP_OK) {
-        ESP_LOGI(INFO_TAG, "HTTP GET Status = %d, content_length = %lld",
+        ESP_LOGI(INFO_TAG, "HTTP GET Status = %d, content_length = %d",
                 esp_http_client_get_status_code(client),
                 esp_http_client_get_content_length(client));
     } else {
