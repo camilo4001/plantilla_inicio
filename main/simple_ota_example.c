c6c86e3bd79a62ca4cf81efc4e5de84a74cfdfb1
camilo.rojas <camilo.rojas@xertica.com>
2022-02-08 18:10:00 -0500

    pruebas

--- main/simple_ota_example.c
+++ main/simple_ota_example.c
@@ -84,7 +84,7 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 		//asprintf(&output_buffer, "%s",evt->data);
 		
 		ESP_LOGI(INFO_TAG, "HTTP_EVENT_ON_DATA, DATOS=%s", output_buffer);
-    }
+    
         break;
     case HTTP_EVENT_ON_FINISH:
         ESP_LOGI(INFO_TAG, "HTTP_EVENT_ON_FINISH");
